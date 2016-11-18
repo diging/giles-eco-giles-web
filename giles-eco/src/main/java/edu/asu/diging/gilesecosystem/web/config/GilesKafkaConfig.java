5e211937aac30da203d530c01729b12655c31889
Julia Damerow <julia.damerow@googlemail.com>
2016-11-18 15:50:52 -0700

    real id for kafka

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/GilesKafkaConfig.java
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/GilesKafkaConfig.java
@@ -39,6 +39,8 @@ public class GilesKafkaConfig implements KafkaConfig {
                 IntegerDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "geco.consumer.giles.1");
+        
         // consumer groups allow a pool of processes to divide the work of
         // consuming and processing records
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "gileseco.web");
