5a7423be5c62b012bd496cafc75237c0b54655e3
Julia Damerow <julia.damerow@googlemail.com>
2016-12-14 09:56:44 +0100

    [GECO-24] fixed DocumentDatabaseClient tests

--- giles-eco/src/test/java/edu/asu/diging/gilesecosystem/web/files/impl/DocumentDatabaseClientTest.java
+++ giles-eco/src/test/java/edu/asu/diging/gilesecosystem/web/files/impl/DocumentDatabaseClientTest.java
@@ -34,8 +34,10 @@ public class DocumentDatabaseClientTest {
     @InjectMocks @Spy private IDocumentDatabaseClient docDatabaseClientToTest;
     
     private final String DOC1_ID = "DOC1";
+    private final String DOC2_ID = "DOC1";
     private final String DOC_ID_NOT_EXIST = "DOC_NOT_EXISTS";
     private final String UPLOAD1_ID = "UP1";
+    private final String USERNAME = "testuser";
     
     @Before
     public void setUp() {
@@ -87,19 +89,147 @@ public class DocumentDatabaseClientTest {
     }
     
     @Test
-    public void test_getDocumentByUploadId_docExists() {
+    public void test_getDocumentByUploadId_docExists() throws Exception {
         IDocument doc1 = new Document();
         doc1.setId(DOC1_ID);
         doc1.setUploadId(UPLOAD1_ID);
         List<IDocument> result = new ArrayList<IDocument>();
         result.add(doc1);
         
-        Mockito.doReturn(result).when(docDatabaseClientToTest, "searchByProperty");
-        Mockito.when(docDatabaseClientToTest).searchby
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                return result;
+            }
+            
+        };
         
-        List<IDocument> searchResult = docDatabaseClientToTest.getDocumentByUploadId(UPLOAD1_ID);
-        Assert.assertEquals(1, doc.size());
-        Assert.assertEquals(UPLOAD1_ID, doc.get(0).getUploadId());
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentByUploadId(UPLOAD1_ID);
+        Assert.assertEquals(1, searchResult.size());
+        Assert.assertEquals(UPLOAD1_ID, searchResult.get(0).getUploadId());
+    }
+    
+    @Test
+    public void test_getDocumentByUploadId_severalDocExists() throws Exception {
+        IDocument doc1 = new Document();
+        doc1.setId(DOC1_ID);
+        doc1.setUploadId(UPLOAD1_ID);
+        
+        IDocument doc2 = new Document();
+        doc2.setId(DOC2_ID);
+        doc2.setUploadId(UPLOAD1_ID);
+        
+        List<IDocument> result = new ArrayList<IDocument>();
+        result.add(doc1);
+        result.add(doc2);
+        
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                return result;
+            }
+            
+        };
+        
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentByUploadId(UPLOAD1_ID);
+        Assert.assertEquals(2, searchResult.size());
+        Assert.assertTrue(searchResult.stream().allMatch(r -> r.getUploadId().equals(UPLOAD1_ID)));
+    }
+    
+    @Test
+    public void test_getDocumentByUploadId_docDoesNotExist() {
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                return new ArrayList<IDocument>();
+            }
+            
+        };
+        
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentByUploadId(UPLOAD1_ID);
+        Assert.assertEquals(0, searchResult.size());
+    }
+    
+    @Test
+    public void test_getDocumentsByUsername_docExists() throws Exception {
+        IDocument doc1 = new Document();
+        doc1.setId(DOC1_ID);
+        doc1.setUsername(USERNAME);
+        
+        List<IDocument> result = new ArrayList<IDocument>();
+        result.add(doc1);
+        
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                return result;
+            }
+            
+        };
+        
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentsByUsername(USERNAME);
+        Assert.assertEquals(1, searchResult.size());
+        Assert.assertEquals(USERNAME, searchResult.get(0).getUsername());
+    }
+    
+    @Test
+    public void test_getDocumentsByUsername_severalDocExists() throws Exception {
+        IDocument doc1 = new Document();
+        doc1.setId(DOC1_ID);
+        doc1.setUsername(USERNAME);
+        
+        IDocument doc2 = new Document();
+        doc2.setId(DOC2_ID);
+        doc2.setUsername(USERNAME);
+        
+        List<IDocument> result = new ArrayList<IDocument>();
+        result.add(doc1);
+        result.add(doc2);
+        
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                return result;
+            }
+            
+        };
+        
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentsByUsername(USERNAME);
+        Assert.assertEquals(2, searchResult.size());
+        Assert.assertTrue(searchResult.stream().allMatch(r -> r.getUsername().equals(USERNAME)));
+    }
+    
+    @Test
+    public void test_getDocumentsByUsername_docDoesNotExists() {
+        // let's mock the superclass method
+        IDocumentDatabaseClient partiallyMockedClient = new DocumentDatabaseClient() {
+
+            @Override
+            protected List<IDocument> searchByProperty(String propName, String propValue,
+                    Class<? extends IDocument> clazz) {
+                // TODO Auto-generated method stub
+                return new ArrayList<IDocument>();
+            }
+            
+        };
+        
+        List<IDocument> searchResult = partiallyMockedClient.getDocumentsByUsername(USERNAME);
+        Assert.assertEquals(0, searchResult.size());
     }
     
     class DocumentIdArgumentMatcher extends ArgumentMatcher<Document> {
