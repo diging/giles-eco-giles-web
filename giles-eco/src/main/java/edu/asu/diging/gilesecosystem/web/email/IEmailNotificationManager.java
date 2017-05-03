c343057b406dccb82d6e9068b2e563c3d7f17c4a
Suraj Nilapwar <suraj.nilapwar@asu.edu>
2017-05-03 09:23:08 -0700

    [GECO-22] review comments changes

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/email/IEmailNotificationManager.java
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/email/IEmailNotificationManager.java
@@ -10,6 +10,15 @@ import edu.asu.diging.gilesecosystem.web.exceptions.GilesNotificationException;
  */
 public interface IEmailNotificationManager {
 
+    /**
+     * Send new account creation email notification to admin
+     *
+     * @param name name of new user
+     * @param username username of new user
+     * @param adminName name of email recipient admin
+     * @param adminEmail email id of recipient admin
+     * @throws GilesNotificationException
+     */
     void sendAccountCreatedEmail(String name, String username, String adminName, String adminEmail)
             throws GilesNotificationException;
 }
