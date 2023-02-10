ccc935d0101a18db6f595aa1147e00e5bc71f7cb
Diya Anna Biju <37469232+diya17@users.noreply.github.com>
2023-02-10 14:15:19 -0700

    GECO-100 : Send emails to users on account activation/revoke (#106)
    * GECO-100 : Initial commit for sending email
    
    * GECO-100: Removing loggers
    
    * GECO-100 : Update the wrong variable
    
    * [GECO-100] addressed review comments: changed the template text, added separate methods and added comments for the interface

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/core/email/impl/EmailNotificationManager.java
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/core/email/impl/EmailNotificationManager.java
@@ -48,9 +48,46 @@ public class EmailNotificationManager implements IEmailNotificationManager {
         } catch (ParseErrorException e) {
             throw new GilesNotificationException(e);
         } catch (Exception e) {
-            //getTemplate method call under getRenderedTemplate can throw exception
             throw new GilesNotificationException(e);
         }
+    }
+    
+    @Override
+    public void sendAccountApprovalEmail(String name, String userName, String userEmail) 
+            throws GilesNotificationException {
+        Map<String, Object> contextProperties = new HashMap<String, Object>();
+        contextProperties.put("name", name);
+        contextProperties.put("userName", userName);
+        try {
+            emailSender.sendNotificationEmail(userEmail, emailMessages.getMessage("email.account_approved.subject", new String[]{}, null), 
+                    velocityBuilder.getRenderedTemplate("velocitytemplates/email/accountApproved.vm", contextProperties));
+        } catch (ResourceNotFoundException e) {
+            throw new GilesNotificationException(e);
+        } catch (ParseErrorException e) {
+            throw new GilesNotificationException(e);
+        } catch (Exception e) {
+            throw new GilesNotificationException(e);
+        }
+        
+    }
+    
+    @Override
+    public void sendAccountRevokedEmail(String name, String userName, String userEmail) 
+            throws GilesNotificationException {
+        Map<String, Object> contextProperties = new HashMap<String, Object>();
+        contextProperties.put("name", name);
+        contextProperties.put("userName", userName);
+        try {
+            emailSender.sendNotificationEmail(userEmail, emailMessages.getMessage("email.account_revoked.subject", new String[]{}, null), 
+                    velocityBuilder.getRenderedTemplate("velocitytemplates/email/accountRevoked.vm", contextProperties));
+        } catch (ResourceNotFoundException e) {
+            throw new GilesNotificationException(e);
+        } catch (ParseErrorException e) {
+            throw new GilesNotificationException(e);
+        } catch (Exception e) {
+            throw new GilesNotificationException(e);
+        }
+        
     }
 
 }
