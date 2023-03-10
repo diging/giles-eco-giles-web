da57832924778aff1b46f84b7af05a5ff5d4ac18
diya17 <diyanna.3@gmail.com>
2023-03-10 16:29:44 -0700

    [GECO-140] Adding new delete document controller to the v2 API

---
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/api/v2/V2DeleteDocumentController.java
@@ -0,0 +1,33 @@
+package edu.asu.diging.gilesecosystem.web.api.v2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import edu.asu.diging.gilesecosystem.web.core.model.IDocument;
+import edu.asu.diging.gilesecosystem.web.core.service.core.ITransactionalDocumentService;
+import edu.asu.diging.gilesecosystem.web.core.service.delete.IDeleteDocumentService;
+
+@Controller
+public class V2DeleteDocumentController {
+
+    @Autowired
+    private IDeleteDocumentService deleteDocumentService;
+    
+    @Autowired
+    private ITransactionalDocumentService documentService;
+    
+    @RequestMapping(value = "/api/v2/resources/documents/{documentId}", method = RequestMethod.DELETE, produces = "application/json;charset=UTF-8")
+    public ResponseEntity<String> reprocessDocument(@PathVariable("documentId") String documentId) {
+        IDocument document = documentService.getDocument(documentId);
+        if (document == null) {
+            return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+        }
+        deleteDocumentService.deleteDocument(document);
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+}
