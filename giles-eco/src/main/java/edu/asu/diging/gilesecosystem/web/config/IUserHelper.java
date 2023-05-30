1235a6b6eba33fba04512c66ed9c35b1f2570743
diya17 <diyanna.3@gmail.com>
2023-05-30 16:59:39 -0700

    [GECO-140] Adding javadocs and test case changes

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/IUserHelper.java
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/IUserHelper.java
@@ -11,9 +11,19 @@ public interface IUserHelper {
     public abstract User createUser(Connection<?> connection);
 
     String createUsername(String username, String providerId);
-
+    
+    /**
+        Checks whether the user associated with the provided Citesphere token has permission to access the given document.
+        @param document The document for which to check the user permission.
+        @param citesphereToken The Citesphere token for the user.
+        @return {@code true} if the user has permission to access the document, {@code false} otherwise.
+    */
     public abstract boolean checkUserPermission(IDocument document, CitesphereToken citesphereToken);
-
+    
+    /**
+        Generates a ResponseEntity with an unauthorized status code (401) and a corresponding error message.
+        @return The generated ResponseEntity containing the unauthorized error message.
+    */
     public abstract ResponseEntity<String> generateUnauthorizedUserResponse();
 
 }
--- giles-eco/src/test/java/edu/asu/diging/gilesecosystem/web/api/v2/V2DeleteDocumentControllerTest.java
+++ giles-eco/src/test/java/edu/asu/diging/gilesecosystem/web/api/v2/V2DeleteDocumentControllerTest.java
@@ -12,6 +12,10 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import edu.asu.diging.gilesecosystem.util.properties.IPropertiesManager;
+import edu.asu.diging.gilesecosystem.web.api.util.IResponseHelper;
+import edu.asu.diging.gilesecosystem.web.config.CitesphereToken;
+import edu.asu.diging.gilesecosystem.web.config.IUserHelper;
 import edu.asu.diging.gilesecosystem.web.core.model.DocumentAccess;
 import edu.asu.diging.gilesecosystem.web.core.model.IDocument;
 import edu.asu.diging.gilesecosystem.web.core.model.impl.Document;
@@ -26,6 +30,15 @@ public class V2DeleteDocumentControllerTest {
     @Mock
     private ITransactionalDocumentService documentService;
     
+    @Mock
+    private IUserHelper userHelper;
+    
+    @Mock
+    private IPropertiesManager propertyManager;
+    
+    @Mock
+    private IResponseHelper responseHelper;
+    
     @InjectMocks
     private V2DeleteDocumentController v2DeleteDocumentController;
     
@@ -33,6 +46,7 @@ public class V2DeleteDocumentControllerTest {
     private DocumentAccess access = DocumentAccess.PRIVATE;
     private String FILE_ID = "fileId";
     private String UPLOAD_ID = "uploadId";
+    private CitesphereToken CITESPHERE_TOKEN = new CitesphereToken("71b9cc36-939d-4e28-89e9-e5bfca1b26c3");
     IDocument document;
     
     @Before
@@ -45,14 +59,23 @@ public class V2DeleteDocumentControllerTest {
     @Test
     public void test_deleteDocument_success() {
         Mockito.when(documentService.getDocument(DOCUMENT_ID)).thenReturn(document);
-        ResponseEntity<String> response = v2DeleteDocumentController.deleteDocument(DOCUMENT_ID);
-        Assert.assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        Mockito.when(userHelper.checkUserPermission(document, CITESPHERE_TOKEN)).thenReturn(true);
+        ResponseEntity<String> response = v2DeleteDocumentController.deleteDocument(DOCUMENT_ID, CITESPHERE_TOKEN);
+        Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+    
+    @Test
+    public void test_deleteDocument_whenUserIsNotAuthorized() {
+        Mockito.when(documentService.getDocument(DOCUMENT_ID)).thenReturn(document);
+        Mockito.when(userHelper.checkUserPermission(document, CITESPHERE_TOKEN)).thenReturn(false);
+        ResponseEntity<String> response = v2DeleteDocumentController.deleteDocument(DOCUMENT_ID, CITESPHERE_TOKEN);
+        Assert.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
     }
     
     @Test
     public void test_deleteDocument_notFound() {
         Mockito.when(documentService.getDocument(DOCUMENT_ID)).thenReturn(null);
-        ResponseEntity<String> response = v2DeleteDocumentController.deleteDocument(DOCUMENT_ID);
+        ResponseEntity<String> response = v2DeleteDocumentController.deleteDocument(DOCUMENT_ID, CITESPHERE_TOKEN);
         Assert.assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
     
