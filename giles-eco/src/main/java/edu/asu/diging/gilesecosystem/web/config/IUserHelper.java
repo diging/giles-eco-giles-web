d0f44d638f3254f9e6787c1d030e256716d3b602
diya17 <diyanna.3@gmail.com>
2023-06-30 09:51:01 -0400

    [GECo-138] Updating unused imports

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/IUserHelper.java
+++ giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/config/IUserHelper.java
@@ -1,9 +1,7 @@
 package edu.asu.diging.gilesecosystem.web.config;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.social.connect.Connection;
 
-import edu.asu.diging.gilesecosystem.web.core.model.IDocument;
 import edu.asu.diging.gilesecosystem.web.core.users.User;
 
 public interface IUserHelper {
