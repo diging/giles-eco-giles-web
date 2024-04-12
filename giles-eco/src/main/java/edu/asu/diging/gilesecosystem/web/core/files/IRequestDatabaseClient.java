9736973633bb60148d23226fc7d863ea6eb25d29
diya17 <diyanna.3@gmail.com>
2024-04-12 12:54:22 -0700

    [GECO-140] Removing unused files

--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/core/files/IRequestDatabaseClient.java
+++
@@ -1,5 +0,0 @@
-package edu.asu.diging.gilesecosystem.web.core.files;
-
-public interface IRequestDatabaseClient {
-    public void deleteRequestsByDocumentId(String documentId);
-}
--- giles-eco/src/main/java/edu/asu/diging/gilesecosystem/web/core/service/core/ITransactionalRequestService.java
+++
@@ -1,5 +0,0 @@
-package edu.asu.diging.gilesecosystem.web.core.service.core;
-
-public interface ITransactionalRequestService {
-    public void deleteRequestsByDocumentId(String documentId);
-}
